8b4a8269221dbd822569b883f98e1340d152d5f9
Matt Goff <matt@goff.us>
2022-12-01 09:32:41 -0600

    cleanup and oop

--- Day1/app.py
+++ Day1/app.py
@@ -1,32 +1,36 @@
-def read_in():
-    with open("./Day1/input_prod.txt", "r") as in_file:
-        return in_file.readlines()
+class ElfClass:
+    def __init__(self, filename):
+        self.file_data = None
+        self.filename = filename
+        self.totals = list()
+        self.read_in()
+        self.parser()
+        self.sorted_totals()
 
+    def read_in(self):
+        with open(self.filename, "r") as in_file:
+            self.file_data = in_file.readlines()
 
-def parser(data):
-    counter = 0
-    totals = []
-    for line in data:
-        if line.strip("\n").isnumeric():
-            counter += int(line)
-        else:
-            totals.append(counter)
-            counter = 0
-    return totals
+    def parser(self):
+        counter = 0
+        for line in self.file_data:
+            if line.strip("\n").isnumeric():
+                counter += int(line)
+            else:
+                self.totals.append(counter)
+                counter = 0
 
+    def sorted_totals(self):
+        self.totals.sort()
 
-def top_three(data):
-    data.sort()
-    return data[-1:-4:-1]
+    def get_top_x(self, count):
+        return sum(self.totals[-1:-count + -1:-1])
 
 
 def main():
-    data = read_in()
-    totals = parser(data)
-    three = top_three(totals)
-    print(totals)
-    print(f"Max = {max(totals)}")
-    print(f"Top Three = {sum(three)}")
+    elf = ElfClass("input_prod.txt")
+    print(f"Max = {elf.get_top_x(1)}")
+    print(f"Top Three = {elf.get_top_x(3)}")
 
 
 if __name__ == "__main__":
